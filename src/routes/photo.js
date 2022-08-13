5370939ff74ee0c7faef2dd289c7df3a5989ade9
aldair-gc <98914031+aldair-gc@users.noreply.github.com>
2022-08-13 15:00:23 -0300

    tratando arquivo enviado

--- src/routes/photo.js
+++ src/routes/photo.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import photoController from '../controllers/Photo';
-import multerConfig from '../config/multer';
 
 const router = new Router();
-const upload = multer(multerConfig);
 
-router.post('/', upload.single('photo'), photoController.store);
+router.post('/', photoController.store);
 
 export default router;
