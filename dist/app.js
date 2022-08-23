ab1f9ccb00b102a1c81a3ff37752e26ef5a86a06
Aldair Garros Carvalho <aldair.gc@outlook.com>
2022-08-23 05:11:54 -0300

    ajuste delay 500ms

--- dist/app.js
+++ dist/app.js
@@ -41,7 +41,7 @@ class App {
   middlewares() {
     this.app.use(_cors2.default.call(void 0, corsOptions));
     // this.app.use(helmet());
-    this.app.use(_expressdelay2.default.call(void 0, 2000));
+    this.app.use(_expressdelay2.default.call(void 0, 500));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads')));
